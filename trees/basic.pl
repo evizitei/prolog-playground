eb9c06ea016bf2d00d5320e0544147518359cf04
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-07-06 16:56:39 -0500

    implement isotrees

--- trees/basic.pl
+++ trees/basic.pl
@@ -4,3 +4,7 @@ btree(tree(_, L, R)) :- btree(L), btree(R).
 tree_member(Element, tree(Element, _, _)).
 tree_member(Element, tree(_, L, _)) :- tree_member(Element, L).
 tree_member(Element, tree(_, _, R)) :- tree_member(Element, R).
+
+iso_tree(void, void).
+iso_tree(tree(X, L, R), tree(X, L1, R1)) :- iso_tree(L, L1), iso_tree(R,R1).
+iso_tree(tree(X, L, R), tree(X, L1, R1)) :- iso_tree(L, R1), iso_tree(R,L1).
