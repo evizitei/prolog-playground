4773b319a98b5507404f6bde76f7e8046b88dce0
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-07-07 09:08:59 -0500

    implement ordered tree exercise from ch3

---
+++ exercises/ch3/ordered_tree.pl
@@ -0,0 +1,20 @@
+ordered_tree(void).
+ordered_tree(tree(X, L, R)) :- ordered_left(X, L), ordered_right(X, R).
+
+ordered_left(_, void).
+ordered_left(X, T) :- ordered_tree(T), greater(X, T).
+
+ordered_right(_, void).
+ordered_right(X, T) :- ordered_tree(T), less(X, T).
+
+greater(_, void).
+greater(X, T) :- max(T, Max), X > Max.
+
+less(_, void).
+less(X, T) :- min(T, Min), X < Min.
+
+max(tree(X, L, void), X) :- ordered_tree(tree(X, L, void)).
+max(tree(X, L, R), MaxR) :- ordered_tree(tree(X, L, R)), max(R, MaxR).
+
+min(tree(X, void, R), X) :- ordered_tree(tree(X, void, R)).
+min(tree(X, L, R), MinL) :- ordered_tree(tree(X, L, R)), min(L, MinL).
