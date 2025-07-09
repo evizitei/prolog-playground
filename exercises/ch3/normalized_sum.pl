e51ac36c9f5e117d18f0c346ce79c1ce1962347e
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-07-09 17:50:35 -0500

    correct normalized sum

--- exercises/ch3/normalized_sum.pl
+++ exercises/ch3/normalized_sum.pl
@@ -1,5 +1,6 @@
+
 nat(0).
-nat(N) :- M is N - 1, M < 100, M >= 0, nat(M).
-constant(N) :- nat(N).
-nrml_sum(X) :- nat(X).
-nrml_sum(X + Y) :- nat(X), nrml_sum(Y).
+nat(s(X)) :- nat(X).
+
+normalized_sum(X) :- nat(X).
+normalized_sum(X + Y) :- nat(X), normalized_sum(Y).
