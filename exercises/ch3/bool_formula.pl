02e271633126a8f95edd1fcccff8d4006c918425
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-07-10 09:56:37 -0500

    implement exercise for cnf detection

--- exercises/ch3/bool_formula.pl
+++ exercises/ch3/bool_formula.pl
@@ -1,8 +1,24 @@
 :- op(200, fy, ~).    % ~ as unary prefix operator
 
 
-bool(true).
-bool(false).
+
+bool_atom(true).
+bool_atom(false).
+
+disjunction(X | Y) :- bool_atom(X), bool_atom(Y).
+
+cnf_element(X) :- bool_atom(X).
+cnf_element(X) :- disjunction(X).
+cnf_element(~X) :- cnf_element(X).
+
+cnf(X) :- cnf_element(X).
+cnf(X ^ Y) :- cnf_element(X), cnf_element(Y).
+
+
+
+
+
+bool(X) :- bool_atom(X).
 bool(~X) :- bool(X).
 bool(X ^ Y) :- bool(X), bool(Y).
 bool(X | Y) :- bool(X), bool(Y).
