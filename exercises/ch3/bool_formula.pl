8cb9e677c910a5602648b270f52e342a67e56ce0
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-07-11 08:04:55 -0500

    implement 'negation_inwards' exercise

--- exercises/ch3/bool_formula.pl
+++ exercises/ch3/bool_formula.pl
@@ -5,11 +5,15 @@
 bool_atom(true).
 bool_atom(false).
 
-disjunction(X | Y) :- bool_atom(X), bool_atom(Y).
+disjunct_element(X) :- bool_atom(X).
+disjunct_element(~X) :- bool_atom(X).
+disjunct_element(X | Y) :- disjunct_element(X), disjunct_element(Y).
+disjunction(X | Y) :- disjunct_element(X), disjunct_element(Y).
 
 cnf_element(X) :- bool_atom(X).
+cnf_element(~X) :- bool_atom(X).
 cnf_element(X) :- disjunction(X).
-cnf_element(~X) :- cnf_element(X).
+
 
 cnf(X) :- cnf_element(X).
 cnf(X ^ Y) :- cnf_element(X), cnf_element(Y).
@@ -22,3 +26,17 @@ bool(X) :- bool_atom(X).
 bool(~X) :- bool(X).
 bool(X ^ Y) :- bool(X), bool(Y).
 bool(X | Y) :- bool(X), bool(Y).
+
+
+negation_inwards(X, X) :- cnf_element(X).
+negation_inwards(~(X ^ Y), (X1 | Y1)) :- bool(X), bool(Y), 
+  negation_inwards(~(X), X1), negation_inwards(~(Y), Y1).
+negation_inwards(~(X | Y), (X1 ^ Y1)) :- bool(X), bool(Y), 
+  negation_inwards(~(X), X1), negation_inwards(~(Y), Y1).
+negation_inwards(~(~X), Y) :- bool(X), negation_inwards(X, Y).
+
+
+
+
+
+
