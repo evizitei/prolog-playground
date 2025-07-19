68a5b1fae3d4943d8b02671a0594f8718999865a
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-07-19 10:54:20 -0500

    make arith programs with directionally specialized use

--- arithmetic/basic.pl
+++ arithmetic/basic.pl
@@ -12,3 +12,9 @@ plus(X, Y, Z) :- nonvar(Z), sample(X), pls(X, Y, Z).
 plus(X, Y, Z) :- nonvar(Y), sample(X), pls(X, Y, Z).
 plus(X, Y, Z) :- nonvar(X), sample(Y), pls(X, Y, Z).
 plus(X, Y, Z) :- var(X), var(Y), var(Z), sample(X), sample(Y), pls(X, Y, Z).
+
+gcd(I, 0, I).
+gcd(I, J, Gcd) :- J > 0, J1 is I mod J, gcd(J, J1, Gcd).
+
+factorial(0, 1).
+factorial(N, F) :- N > 0, N1 is N - 1, factorial(N1, F1), F is N * F1.
