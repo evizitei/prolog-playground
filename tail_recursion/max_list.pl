a5afc03039964cc12e40992de2f4196b2e5d677f
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-07-27 08:04:21 -0500

    extract auxillary predicate

--- tail_recursion/max_list.pl
+++ tail_recursion/max_list.pl
@@ -1,9 +1,8 @@
 max_list(Xs, X) :- max_list(Xs, 0, X).
 max_list([], X, X).
 max_list([X|Xs], PrevMax, Max) :- 
-  X > PrevMax,
-  max_list(Xs, X, Max).
-  
-max_list([X|Xs], PrevMax, Max) :-
-  X =< PrevMax,
-  max_list(Xs, PrevMax, Max).
+  maximum(X, PrevMax, M),
+  max_list(Xs, M, Max).
+
+maximum(X,Y,Y) :- X =< Y.
+maximum(X,Y,X) :- X > Y.
