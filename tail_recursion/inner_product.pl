77ccf3eb013681b629f8c6a59611e7b67e544f95
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-07-27 07:21:36 -0500

    implement inner product with accumulator

--- tail_recursion/inner_product.pl
+++ tail_recursion/inner_product.pl
@@ -1,2 +1,8 @@
 iprod([],[],0).
 iprod([X|Xs],[Y|Ys],IP) :- iprod(Xs, Ys, P), IP is P + (X * Y).
+
+itprod([],[], IP, IP).
+itprod([X|Xs],[Y|Ys], Temp, IP) :- 
+  T1 is Temp + (X * Y), 
+  itprod(Xs, Ys, T1, IP).
+itprod(Xs, Ys, IP) :- itprod(Xs, Ys, 0, IP).
