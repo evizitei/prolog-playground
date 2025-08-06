da2eaad836cc43c64df8e9e6d8b9033b252531c8
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-08-06 10:15:39 -0500

    remove temporary artifacts

--- task_experiments/hole_wall_63453dd6/task.pl
+++
@@ -1,9 +0,0 @@
-
-rows(16).
-cols(16).
-% there is a blue wall in the task somewhere
-divider(11).
-% the whole grid to the right of the vertical wall is empty of shapes in the input.
-% the shapes are all rigidly translated right until they touch the blue wall.
-% every "enclosed hole" in a shape that has a border directly touching the blue wall transforms
-% the 
